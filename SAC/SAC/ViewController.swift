553dd674e9ebe9351fe47d9206ca92485f19b88f
Bala <imba7a@gmail.com>
2017-08-19 11:33:43 +0530

    Initial project setup

---
+++ SAC/SAC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SAC
+//
+//  Created by Bala on 19/08/17.
+//  Copyright © 2017 ShopAroundTheCorner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
