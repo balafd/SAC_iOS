6793828de136a6a5ea33beb2e6f61fa6a8f110bf
girish <girishkoundinya@Koundinyas-Mac.local>
2017-08-20 07:36:12 +0530

    Added forms

--- SAC/SAC/RegisterViewController.swift
+++ SAC/SAC/RegisterViewController.swift
@@ -7,11 +7,41 @@
 //
 
 import UIKit
+import SwiftForms
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.separatorStyle = .none
+        createForm()
+    }
+    
+    func createForm(){
+        var form = FormDescriptor()
+        form.title = "Example form"
+        
+        // Define first section
+        var section1 = FormSectionDescriptor(headerTitle: "Create shop", footerTitle: "");
+        
+        var row = FormRowDescriptor(tag: "name", type: .email, title: "Name")
+        section1.rows.append(row)
+        
+        row = FormRowDescriptor(tag: "phone", type: .phone, title: "Phone Number")
+        section1.rows.append(row)
+
+        row = FormRowDescriptor(tag: "address", type: .phone, title: "Address")
+        section1.rows.append(row)
+
+        row = FormRowDescriptor(tag: "owner", type: .phone, title: "Owner")
+        section1.rows.append(row)
+        
+        row = FormRowDescriptor(tag: "button", type: .button, title: "Submit")
+        section1.rows.append(row)
+        
+        form.sections = [section1]
+        
+        self.form = form
     }
 
     override func didReceiveMemoryWarning() {
