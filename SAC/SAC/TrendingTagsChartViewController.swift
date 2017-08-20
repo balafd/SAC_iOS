015edebb825cff193a5a6cba8d022aa305962c62
Bala <imba7a@gmail.com>
2017-08-20 12:57:15 +0530

    Add Chart

--- SAC/SAC/TrendingTagsChartViewController.swift
+++ SAC/SAC/TrendingTagsChartViewController.swift
@@ -11,15 +11,27 @@ import Charts
 
 class TrendingTagsChartViewController: UIViewController {
 
-    @IBOutlet weak var barChartView: BarChartView!
+    @IBOutlet weak var chartView: LineChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let point1 = CGPoint(x: 0, y: 12)
+        let point2 = CGPoint(x: 1, y: 16)
+        let point3 = CGPoint(x: 3, y: 15)
+        let point4 = CGPoint(x: 4, y: 13)        
+        let numbers = [point1, point2, point3, point4]
+        var lineChartEntry = [ChartDataEntry]()
         
-        
-        
+        for point in numbers {
+            let chartData = ChartDataEntry(x: Double(point.x), y: Double(point.y))
+            lineChartEntry.append(chartData)
+        }
+        let line = LineChartDataSet(values: lineChartEntry, label: "Trends")
+        let data = LineChartData()
+        data.addDataSet(line)
+        chartView.data = data
     }
 
     override func didReceiveMemoryWarning() {
