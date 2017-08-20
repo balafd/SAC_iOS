95a7fb159c97f5d652c6a4507f928ba4e0f55a60
girish <girishkoundinya@Koundinyas-Mac.local>
2017-08-20 08:39:28 +0530

    Added view shop

--- SAC/Pods/Target Support Files/Pods-SAC/Pods-SAC-frameworks.sh
+++ SAC/Pods/Target Support Files/Pods-SAC/Pods-SAC-frameworks.sh
@@ -90,11 +90,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Charts/Charts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftForms/SwiftForms.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Charts/Charts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftForms/SwiftForms.framework"
 fi
